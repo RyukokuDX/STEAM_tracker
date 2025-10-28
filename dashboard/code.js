0aedb4f6025daa4870258118522fa85da2c05531
shimizu-kiichi <y240152@mail.ryukoku.ac.jp>
2025-10-28 14:13:35 +0900

    add(dashboard): スプレッドシート全体の情報をWebアプリに表示
    # スプレッドシート全体の情報をWebアプリに表示する
    
    code.gsでスプレッドシートの特定のシート全体の文字列を取得して、Webアプリで表示できるようにする。
    
    - `getDataRange`と`getValues()`を使ってシート全体の文字列を取得
    - `doGet()`でテンプレートHTMLにデータを埋め込み、Webアプリ上に表示

--- dashboard/code.js
+++ dashboard/code.js
@@ -1,16 +1,14 @@
 // Code.gs
 const SPREADSHEET_ID = "1U3enu3ETOh_seroYmpO95p2yjW2RSUC7WRLyDk1rhs8";
 const ss = SpreadsheetApp.openById(SPREADSHEET_ID);
-const sh = ss.getSheetByName("シート1");
+const sh = ss.getSheetByName("シート1"); 
 
-const c2 = sh.getRange(2, 3).getValue();
-
-function myFunction() {
-  //test
+function getSheetData() {
+    return sh.getDataRange().getValues();
 }
 
 function doGet() {
   const tpl = HtmlService.createTemplateFromFile('index');
-  tpl.name = c2;
+  tpl.data = getSheetData();
   return tpl.evaluate().setTitle("ダッシュボード");
 }
