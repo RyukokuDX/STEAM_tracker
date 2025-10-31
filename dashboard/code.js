e437a6635103ab6ca0ac3cb0177e6626b9575cdb
shimizu-kiichi <y240152@mail.ryukoku.ac.jp>
2025-10-31 23:54:50 +0900

    add(dashboard): 特定の列を非表示にする
    # Webアプリに表示するテーブルから、指定した列を除外する機能を追加
    
    - `doGet()`で非表示対象の列番号をHTMLテンプレートに渡す
    - `HTML`テンプレート側で列番号を判定し、対象の `<th>` (ヘッダー) と `<td>` (セル) を描画しないよう条件分岐を追加

--- dashboard/code.js
+++ dashboard/code.js
@@ -1,7 +1,15 @@
 // Code.gs
 const SPREADSHEET_ID = "1U3enu3ETOh_seroYmpO95p2yjW2RSUC7WRLyDk1rhs8";
 const ss = SpreadsheetApp.openById(SPREADSHEET_ID);
-const sh = ss.getSheetByName("シート1"); 
+const sn = "管理シート";
+
+const registered = 1;
+const email = 2;
+const DriveLinkColum = 5;
+const handover = 6;
+const days_until_handover = 7;
+
+const sh = ss.getSheetByName(sn);
 
 function getSheetData() {
     return sh.getDataRange().getValues();
@@ -10,5 +18,11 @@ function getSheetData() {
 function doGet() {
   const tpl = HtmlService.createTemplateFromFile('index');
   tpl.data = getSheetData();
+  tpl.sheetName = sn;
+  tpl.registered = registered;
+  tpl.mail = email;
+  tpl.photo = DriveLinkColum;
+  tpl.handover = handover;
+  tpl.days = days_until_handover;
   return tpl.evaluate().setTitle("ダッシュボード");
 }
