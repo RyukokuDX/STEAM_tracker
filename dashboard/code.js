8212a9961b55857eaada1775e9546457b784050a
shimizu-kiichi <example001@mail.ryukoku.ac.jp>
2025-11-02 23:31:03 +0900

    管理者用ダッシュボードの試作を追加 (#37)
    * add(dashboard): 管理者用ダッシュボード(Webアプリ)を追加
    
    # スプレッドシートの文字列を取得して、Webアプリ上に表示する
    
    最終的に、管理者用ダッシュボードでスプレッドシートを管理する。そのため、スプレッドシートのセルの文字列をGASのWebアプリで表示できるか確認する。
    
    - `SpreadsheetApp`を使用して対象のシートにアクセス
    - `getRange()`と`getValue()`を使って特定のセルの文字列を取得
    - `doGet()`でテンプレートHTMLにデータを埋め込み、Webアプリ上に表示
    
    * add(dashboard): スプレッドシート全体の情報をWebアプリに表示
    
    # スプレッドシート全体の情報をWebアプリに表示する
    
    code.gsでスプレッドシートの特定のシート全体の文字列を取得して、Webアプリで表示できるようにする。
    
    - `getDataRange`と`getValues()`を使ってシート全体の文字列を取得
    - `doGet()`でテンプレートHTMLにデータを埋め込み、Webアプリ上に表示
    
    * add(dashboard): 特定の列を非表示にする
    
    # Webアプリに表示するテーブルから、指定した列を除外する機能を追加
    
    - `doGet()`で非表示対象の列番号をHTMLテンプレートに渡す
    - `HTML`テンプレート側で列番号を判定し、対象の `<th>` (ヘッダー) と `<td>` (セル) を描画しないよう条件分岐を追加
    
    * add(dashboard): 画像の表示に対応
    
    # スプレッドシートに記録されたGoogle DriveのidからURLを作成し、Webアプリで画像を表示させる
    
    - `doGet()`でidをHTMLテンプレートに渡す
    - "https://lh3.googleusercontent.com/d/"の後にidを付け加えたURLを作成
    - `img`タグで生成したURLを使い、画像を表示
    
    * add(dashboard): 画像の表示に対応
    
    # スプレッドシートに記録されたGoogle DriveのidからURLを作成し、Webアプリで画像を表示させる
    
    - `doGet()`でidをHTMLテンプレートに渡す
    - "https://lh3.googleusercontent.com/d/"の後にidを付け加えたURLを作成
    - `img`タグで生成したURLを使い、画像を表示
    
    * add: demo-page
    
    * add(dashboard): Enhance demo page with new button layout and email tooltip
    
    - Introduced a header container with buttons for archiving and action groups (complete, discard, delete).
    - Added styling for buttons with hover effects for better user interaction.
    - Implemented tooltips displaying email addresses on hover for specific table cells.
    
    * refactor(dashboard): copilotによる修正を反映
    add(dashboard): index-demo.htmlの一部をindex.htmlに反映
    
    * refactor(dashboard): copilotによる修正を反映
    add(dashboard): index-demo.htmlの一部をindex.htmlに反映
    
    ---------
    
    Co-authored-by: KazumasaFUJIWARA <fujiwara.kazumasa@math.ryukoku.ac.jp>

---
+++ dashboard/code.js
@@ -0,0 +1,30 @@
+// Code.gs
+const SPREADSHEET_ID = PropertiesService.getScriptProperties().getProperty('SPREADSHEET_ID');
+const ss = SpreadsheetApp.openById(SPREADSHEET_ID);
+const sn = "管理シート";
+
+const registered = 1;
+const email = 2;
+const name = 3;
+const DriveLinkColumn = 5;
+const handover = 6;
+const days_until_handover = 7;
+
+const sh = ss.getSheetByName(sn);
+
+function getSheetData() {
+  return sh.getDataRange().getValues();
+}
+
+function doGet() {
+  const tpl = HtmlService.createTemplateFromFile('index');
+  tpl.data = getSheetData();
+  tpl.sheetName = sn;
+  tpl.registered = registered;
+  tpl.email = email;
+  tpl.name = name;
+  tpl.photo = DriveLinkColumn;
+  tpl.handover = handover;
+  tpl.days = days_until_handover;
+  return tpl.evaluate().setTitle("ダッシュボード");
+}
