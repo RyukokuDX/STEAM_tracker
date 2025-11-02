bb1cfaa7f7f3f62aaaed5231e426bf93a428a0e9
shimizu-kiichi <y240152@mail.ryukoku.ac.jp>
2025-11-02 23:29:03 +0900

    refactor(dashboard): copilotによる修正を反映 add(dashboard): index-demo.htmlの一部をindex.htmlに反映

--- dashboard/code.js
+++ dashboard/code.js
@@ -1,18 +1,19 @@
 // Code.gs
-const SPREADSHEET_ID = "1U3enu3ETOh_seroYmpO95p2yjW2RSUC7WRLyDk1rhs8";
+const SPREADSHEET_ID = PropertiesService.getScriptProperties().getProperty('SPREADSHEET_ID');
 const ss = SpreadsheetApp.openById(SPREADSHEET_ID);
 const sn = "管理シート";
 
 const registered = 1;
 const email = 2;
-const DriveLinkColum = 5;
+const name = 3;
+const DriveLinkColumn = 5;
 const handover = 6;
 const days_until_handover = 7;
 
 const sh = ss.getSheetByName(sn);
 
 function getSheetData() {
-    return sh.getDataRange().getValues();
+  return sh.getDataRange().getValues();
 }
 
 function doGet() {
@@ -21,7 +22,8 @@ function doGet() {
   tpl.sheetName = sn;
   tpl.registered = registered;
   tpl.email = email;
-  tpl.photo = DriveLinkColum;
+  tpl.name = name;
+  tpl.photo = DriveLinkColumn;
   tpl.handover = handover;
   tpl.days = days_until_handover;
   return tpl.evaluate().setTitle("ダッシュボード");
