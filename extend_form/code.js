e8920174c983ffac8517282352e32eb97a9dcbbe
haruki0405 <y240243@mail.ryukoku.ac.jp>
2026-01-26 13:28:41 +0900

    add(extend_form): メール本文の内容を修正

--- extend_form/code.js
+++ extend_form/code.js
@@ -257,7 +257,8 @@ function approveRequest(id, newDate) {
 
   // 申請者にメール送信
   const subject = `STEAMコモンズ 延長申請許可通知`;
-  const body = `${name}さん（${organ}）\n\n延長申請が許可されました。\n新しい明け渡し日: ${newDate}`;
+  const body = `${name}さん（${organ}）\n\n延長申請が許可されました。\n新しい明け渡し日: ${newDate}\n\n
+                このメッセージに心当たりがない場合は、STEAMコモンズまでお越しください。`;
   sendEmail(email, subject, body);
    
   sendLineMessage(USER_ID, `延長申請No.${id}を ${newDate} まで許可しました。`);
@@ -280,7 +281,8 @@ function rejectRequest(id) {
 
   // 申請者にメール送信
   const subject = `STEAMコモンズ 延長申請却下通知`;
-  const body = `${name}さん（${organ}）\n\n延長申請が却下されました。\n`;
+  const body = `${name}さん（${organ}）\n\n延長申請が却下されました。\n\n
+                このメッセージに心当たりがない場合は、STEAMコモンズまでお越しください。`;
   sendEmail(email, subject, body);
 
   sendLineMessage(USER_ID, `延長申請No.${id}を却下しました。`);
